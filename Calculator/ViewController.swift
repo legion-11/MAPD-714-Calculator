6f4a56e2fb175976a70348def7b8868d8a649b44
legion-11 <legalionion@gmail.com>
2020-09-28 22:39:05 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by legion-11 on 28.09.2020.
+//  Copyright © 2020 legion-11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
