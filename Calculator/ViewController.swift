23263a607fa28f1979ef1ce4c88443b131155544
legion-11 <legalionion@gmail.com>
2020-10-06 20:26:51 +0300

    input Numbers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,9 +15,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
     
+    
+    @IBOutlet weak var inputLable: UILabel!
+    
+    var initTyping = true;
+    var startedWritingOperand = true;
+    var pointTyped = false;
+    var firstOperand: Double = 0
+    var currentInputedOperand: String = "";
+    
+    func addString(_ str: String){
+        currentInputedOperand += str
+        inputLable.text! += str;
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!;
+        
+        if !startedWritingOperand{
+            addString(number);
+        }else if number != "0"{
+            if initTyping {
+                inputLable.text! = "";
+                initTyping = false
+            }
+            addString(number);
+            startedWritingOperand = false;
+            
+        }
+    }
+    
+    @IBAction func OperatorPressed(_ sender: UIButton) {
+        firstOperand = Double(currentInputedOperand)!;
+        currentInputedOperand = "";
+        startedWritingOperand = true
+        
+    }
 }
 
