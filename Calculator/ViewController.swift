a142b9ff014410c67c25f7c04b34ab11b0d32c22
legion-11 <legalionion@gmail.com>
2020-10-09 18:52:20 +0300

    assignment 2 done

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,6 +6,8 @@
 //  Student number 301132978
 //  on 26.09.2020.
 //
+//Simple calculator project without operations priority,
+//so + operation calculated before * if it's goes first
 //
 
 import UIKit
@@ -20,42 +22,77 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLable: UILabel!
     @IBOutlet weak var inputLable: UILabel!
     
-    var splitedInput = ["0"]
+    var splitedInput = ["0"]  // array of strings in order operand, operator exmple: ["1", "+", "1.232", "-"...]
     
-    @IBAction func buttonPressed(_ sender: UIButton) {
+    // calculate operation between two operands
+    func calculate1Operation(_ first: Double, _ second: Double, _ operation:String) -> String{
+        switch operation {
+        case "%":
+            return String(Int(first) % Int(second));
+        case "+":
+            return String (first+second);
+        case "–":
+            return String (first-second);
+        case "÷":
+            if second == 0.0 {return "error"}
+            return String (first/second);
+        case "✕":
+            return String (first*second);
+        default:
+            print("error");
+            return "error"
+        }
+    }
+    /*
+     compound calculation in for loop by reasigning result variable with new calculation
+     between result and next operand in array
+     example splitedInput = ["1",+,"2",-,"3"]
+     1 result = 1;
+     2 result = result + 2; // 3
+     3 result = result - 3; // 0
+     */
+    func calculateFull(){
+        var result = Double(splitedInput[0]);
+        for i in stride(from: 2, to: splitedInput.count, by: 2) {
+            let tmp = calculate1Operation(Double(result!), Double(splitedInput[i])!, splitedInput[i-1]);
+            if !(tmp == "error"){
+                result! = Double(tmp)!;
+            }else{
+                print("error");
+                resultLable.text! = "error";
+                return
+            }
+        }
+        
+        resultLable.text! = "=" + String(format: "%.9f",result!);
+    }
+    
+    /*
+     listener to 0-9 buttons
+     appends splitedInput with new operand
+     or modify last one, if the last element in array is operand
+     won't allow input integer that starts with 0
+     */
+    @IBAction func numberPressed(_ sender: UIButton) {
         let number = sender.currentTitle!;
         let last = splitedInput.last;
         
         if (last == "0"){
             splitedInput[splitedInput.endIndex-1] = number;
         }else if (["✕", "%", "+", "–", "÷"].contains(last)){
-            splitedInput.append(number)
+            splitedInput.append(number);
         }else {
-            splitedInput.append(splitedInput.removeLast() + number);
+            splitedInput[splitedInput.count-1] += number;
         }
         inputLable.text! = splitedInput.joined();
+        calculateFull();
     }
     
-
-    func calculate(_ first: Double, _ second: Double, _ operation:String) -> String{
-        switch operation {
-        case "%":
-             return String(Int(first) % Int(second))
-        case "+":
-            return String (first+second)
-        case "–":
-            return String (first-second)
-        case "÷":
-            if second == 0.0 {return "error"}
-            return String (first/second)
-        case "✕":
-            return String (first*second)
-        default:
-            print("error")
-            return "error"
-        }
-    }
-    
+    /*
+     listener to ✕, %, +, –, ÷ buttons
+     appends splitedInput with new operator
+     or modify one, if the last element in array is operator
+     */
     @IBAction func OperatorPressed(_ sender: UIButton) {
         let operatorSign = sender.currentTitle!;
         if (["✕", "%", "+", "–", "÷"].contains(splitedInput.last)){
@@ -66,59 +103,76 @@ class ViewController: UIViewController {
         inputLable.text! = splitedInput.joined();
     }
     
+    /*
+     listener to +/- button
+     changes the sign of last operator if it goes last in splitedInput and not equals 0
+     */
     @IBAction func pressChangeSign(_ sender: UIButton) {
-        if !(["✕", "%", "+", "–", "÷"].contains(splitedInput.last) || splitedInput.last != "0"){
+        if !(["✕", "%", "+", "–", "÷"].contains(splitedInput.last) || Double(splitedInput.last!) == 0){
             if (splitedInput.last!.first != "-"){
-                splitedInput.append("-" + splitedInput.removeLast());
+                splitedInput[splitedInput.count-1] = "-" + splitedInput[splitedInput.count-1];
             }else{
                 splitedInput[splitedInput.endIndex-1].removeFirst();
             }
             inputLable.text! = splitedInput.joined();
+            calculateFull();
         }
     }
     
+    /*
+     listener to <- (backspace) button
+     depending on what goes last in splitedInput
+     removes last char of last operator (if it has more than 1 digit, otherwise removes it completely)
+     or removes last operation from splitedInput
+     not allowing to have empty splitedInput
+     */
     @IBAction func pressBackspace(_ sender: UIButton) {
         if (splitedInput.last!.count == 1){
             splitedInput.removeLast();
             if (splitedInput.count == 0){
-                splitedInput.append("0")
+                splitedInput.append("0");
             }
         }else{
             splitedInput[splitedInput.endIndex-1].removeLast();
         }
         inputLable.text! = splitedInput.joined();
+        calculateFull();
     }
     
+    /*
+     listener to . (dot) button
+     inputs dot in last operator if it don't have one
+     if last element in splitedInput is operation sign than appends it with "0."
+     */
     @IBAction func pressDot(_ sender: UIButton) {
         let last = splitedInput.last;
         
         if (["✕", "%", "+", "–", "÷"].contains(last)){
-            splitedInput.append("0.")
+            splitedInput.append("0.");
         }else if !((last?.contains("."))!){
-            splitedInput.append(splitedInput.removeLast() + ".");
+            splitedInput[splitedInput.count-1] += ".";
         }
         inputLable.text! = splitedInput.joined();
     }
     
+    /*
+     listener to = (equals) button
+     rewrite splitedInput with text of resultLable
+     */
     @IBAction func pressEquals(_ sender: UIButton) {
-        if (["✕", "%", "+", "–", "÷"].contains(splitedInput.last)){
-            splitedInput.removeLast();
-        }
-        var result = Double(splitedInput[0]);
-        for i in stride(from: 2, to: splitedInput.count, by: 2) {
-            let tmp = calculate(Double(result!), Double(splitedInput[i])!, splitedInput[i-1]);
-            if !(tmp == "error"){
-                result! = Double(tmp)!;
-                print(i, result!)
-            }else{
-                print("error");
-                resultLable.text! = "error";
-            }
-        }
-        print(result!)
-        resultLable.text! = String(result!);
+        //remove = (equals) char
+        var resultStr = resultLable.text!;
+        resultStr.removeFirst()
+        
+        splitedInput = [resultStr];
+        inputLable.text! = splitedInput.joined();
     }
     
+    /*
+     listener to AC button
+     rewrite splitedInput with ["0"]
+     do not affect resultLable in case if user wants ro copy result in input
+     */
     @IBAction func pressAllClear(_ sender: UIButton) {
         splitedInput = ["0"]
         inputLable.text! = splitedInput.joined();
